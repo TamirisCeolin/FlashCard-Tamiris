535e0ac6b8a6072fbb0d6b128a357411d076440e
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-10-09 09:02:06 -0300

    09/10

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao';
 
     cartao.innerHTML = `
-<div class="cartao__conteudo">
-<h3>Programação</h3>
-<div class="cartao__conteudo__pergunta">
+    <div class="cartao__conteudo">
+    <h3>Programação</h3>
+    <div class="cartao__conteudo__pergunta">
         <p>O que é JavaScript?</p>
-</div>
-<div class="cartao__conteudo__resposta">
+    </div>
+    <div class="cartao__conteudo__resposta">
         <p>O JavaScript é uma linguagem de programação</p>
-</div>
-</div>
-`
+    </div>
+    </div>
+    `
 
     container.appendChild(cartao);
 }
